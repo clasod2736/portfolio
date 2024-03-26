9b1bf0417821f5cc22d4b4c8c61f0a262319a036
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2024-03-26 23:14:13 +1100

    fix main heights

--- src/Pages/home/index.tsx
+++ src/Pages/home/index.tsx
@@ -3,7 +3,7 @@ import Logo from "../../components/home/Logo";
 
 export default function Home() {
   return (
-    <section className=" z-[-10] min-[320px]:h-[90%] md:h-[60%] lg:h-[70%] flex flex-col justify-center items-center gap-6">
+    <section className=" z-[-10] h-[80%] md:h-[60%] lg:h-[70%] flex flex-col justify-center items-center gap-6">
       <Logo />
       <Intro />
     </section>
