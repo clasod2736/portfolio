a32ea7d264e6899178aeacf0aabe0e95dcbe519f
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2024-03-28 11:03:46 +1100

    add "/" to front of every route in App.tsx file & delete "/experience/*" route

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,10 @@ const router = createBrowserRouter([
     errorElement: <NotFound />,
     children: [
       { index: true, element: <Home /> },
-      { path: "about", element: <About /> },
-      { path: "experience", element: <Experience /> },
-      { path: "experience/*", element: <Experience /> },
-      { path: "projects", element: <Projects /> },
-      { path: "contact", element: <Contact /> },
+      { path: "/about", element: <About /> },
+      { path: "/experience", element: <Experience /> },
+      { path: "/projects", element: <Projects /> },
+      { path: "/contact", element: <Contact /> },
     ],
   },
 ]);
