01682393eae258c74b20186bb37b40cc8201502a
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2024-04-09 20:44:05 +1000

    change resume and background

--- src/components/about/Background.tsx
+++ src/components/about/Background.tsx
@@ -4,20 +4,19 @@ export default function Background() {
       <h4 className="py-2 border-b-[1px] border-zinc-500 md:border-b-0 w-full md:w-[20%] md:text-right text-3xl md:text-2xl xl:text-[32px] animate-slideToRight animation-delay-200">
         Background
       </h4>
-      <p className="md:pt-0 min-[320px]:pt-2 px-0 md:px-4 lg:px-8 min-[500px]:text-lg lg:text-xl leading-8 w-full md:w-[80%] flex-wrap animate-[slideUp_0.3s]">
+      <p className="md:pt-0 min-[320px]:pt-2 px-0 md:px-8 min-[500px]:text-lg lg:text-xl leading-8 w-full md:w-[80%] flex-wrap animate-[slideUp_0.3s]">
         Hi! I'm <b>Joon</b>, based in Melbourne and have been self-studying
         Software Engineering since early 2023. <br /> <br />
-        As a web developer, I enjoy — combining my technical knowledge with my
-        keen eye for details to create a solid functioning website. <br />
-        My goal is to always build applications that are <b>
-          scalable
-        </b> and <b>efficient</b> under the hood while providing engaging,
-        pixel-perfect user experiences. <br />
-        Currently, my focus lives on <b>Front-End</b>, which I find incredibly
-        fulfilling. <br /> <br />
-        To achieve my goal, I always approach coding with{" "}
+        I've focused my journey on front-end and full-stack development through{" "}
+        <b>three personal projects.</b>
+        <br />
+        My goal is to build skillset and insight to create web applications that
+        are <b>scalable</b> and <b>efficient</b> under the hood while providing
+        engaging, pixel-perfect user experiences. <br />
+        <br />
+        To achieve that, I always approach coding with{" "}
         <b>multiple strategies,</b>
-        {""} <b>keen eyes for detail</b>
+        {""} <b>a keen eye for detail</b>
         {""} and <b>creativity</b>
         <br />
         <br />
