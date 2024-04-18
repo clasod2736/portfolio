d0d026f5e405ec50edfcd98c8f8ee5fd7db8b10d
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2024-04-18 16:08:43 +1000

    added preloading in project page

--- src/Pages/projects/index.tsx
+++ src/Pages/projects/index.tsx
@@ -4,7 +4,11 @@ import ProjectsList from "../../components/projects/ProjectsList";
 import ProjectsImg from "../../components/projects/ProjectsImg";
 import ProjectsBrief from "../../components/projects/ProjectsBrief";
 
+import { preloadingImg } from "../../util/preloadingImg";
+
 export default function Projects() {
+  preloadingImg();
+
   return (
     <section className="flex flex-col md:grid md:justify-items-center md:grid-cols-[32%_65%] lg:grid-cols-[70%_30%] grid-rows-[65%_30%] lg:grid-rows-[45%_55%] justify-start md:justify-between items-center w-[95%] lg:w-[85%] h-[90%] md:h-[70%] lg:h-[60%] gap-6">
       <ProjectProvider>
